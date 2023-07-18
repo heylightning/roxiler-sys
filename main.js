8b6e8e5d416476bc1c6d42bb4629309134df29f8
Pratham <mishrapratham.codeit@gmail.com>
2023-07-18 22:09:14 +0530

    v0.0.2

---
+++ main.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+fetch("https://s3.amazonaws.com/roxiler.com/product_transaction.json", {
+  method: "GET",
+})
+  .then((response) => response.json())
+  .then((data) => {
+    app.get("/api", (req, res) => {
+      res.json(data[0]);
+    });
+  })
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.json({ health_check: "passed" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
